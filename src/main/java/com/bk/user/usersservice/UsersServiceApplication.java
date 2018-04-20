925bf2c78121ce2640b0ad4de9396bef198ca92c
Billy Korando <bkorando@keyholesoftware.com>
2018-04-19 21:27:43 -0500

    initial import

---
+++ src/main/java/com/bk/user/usersservice/UsersServiceApplication.java
@@ -0,0 +1,12 @@
+package com.bk.user.usersservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersServiceApplication.class, args);
+	}
+}
